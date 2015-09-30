8bfc90b6fbbb1dbca8c415d7aedf788c98976f21
shimabox <shimabox@gmail.com>
2015-10-01 07:52:09 +0900

    onmessageをglobalにしないと、chrome(PC)で呼ばれなくなっていた ※自分が気づいたのはバージョン 45.0.2454.101 (64-bit)だけど、結構前からダメだったっぽい

--- js/worker/readFileWorker.js
+++ js/worker/readFileWorker.js
@@ -1,17 +1,18 @@
-var helper = {
+var _helper = {
     debug : function() {
         postMessage({'evtName':'debug', 'echo': arguments});
     }
-}
+};
 
-var onmessage = function(e) {
+onmessage = function(e) {
     var reader = new FileReader(),
         file = e.data.file,
         index = e.data.index;
 
     reader.onloadend = function(e) {
         postMessage({'index': index, 'evtName': 'loadend', 'src': e.target.result});
-    }
+        // _helper.debug(file, index);
+    };
 
     reader.readAsDataURL(file);
-}
+};
